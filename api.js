e93f200622ff0aff7a2194f06573e076e94f9d86
SilentCrouwd <chris.buchholz.lauenau@googlemail.com>
2026-06-07 17:45:52 +0200

    Implement Cathegories implement Joke serch with Cathegorie fetch Joke  Categories Set Categorie Deopdown change Styls for Deopdwon and make it responsive

--- api.js
+++ api.js
@@ -1,10 +1,33 @@
-const API_ENDPOINT = "https://witzapi.de//api/joke/";
+const API_ENDPOINT = "https://witzapi.de/api/joke/";
+const API_CATHEGORIE = "https://witzapi.de/api/category/";
+
+const API_ENDPOINT_CATHEGORY =
+  "https://witzapi.de/api/joke/?limit=1&category=antiwitze&language=de";
 export let jokeList = [];
 
-export async function getJoke() {
+export async function getCategory() {
+  let category = [];
+  const response = await fetch(API_CATHEGORIE);
+  category = await response.json();
+  return category;
+}
+
+export async function getJoke(category) {
+  if (category === "Kategorie" || !category) {
+    const response = await fetch(API_ENDPOINT);
+    const newJoke = await response.json();
+    return newJoke[0].text;
+  } else {
+    const response = await fetch(
+      `https://witzapi.de/api/joke/?limit=1&category=${category}&language=de`,
+    );
+    const newJoke = await response.json();
+    return newJoke[0].text;
+  }
+
   const response = await fetch(API_ENDPOINT);
   const newJoke = await response.json();
-  return newJoke;
+  return newJoke[0].text;
 }
 
 export function setLocalStorage() {
@@ -16,4 +39,9 @@ export function getLocalStorage() {
     jokeList = JSON.parse(localStorage.getItem("jokes"));
     return jokeList;
   }
+}
+
+export function removeJoke(index) {
+  jokeList.splice(index, 1);
+  setLocalStorage();
 }
